4c45ec5651c817fa381221dcd06d61f9cd0aea4d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-09 10:36:00 +0600

    is object empty

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+var isEmpty = function (obj) {
+  console.log(Object.keys(obj).length);
+  if (Object.keys(obj).length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const result = isEmpty({});
+
+console.log("result=>", result);
