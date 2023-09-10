ab1e704ff3c5a0cb29eb3b729a7fbc2a32e8e85b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-10 09:21:09 +0600

    chunk array with iterative solution

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,22 @@
-var isEmpty = function (obj) {
-  console.log(Object.keys(obj).length);
-  if (Object.keys(obj).length == 0) {
-    return true;
-  } else {
-    return false;
+var chunk = function (arr, size) {
+  const result = [];
+  let subArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    subArr.push(arr[i]);
+    if (subArr.length === size) {
+      result.push(subArr);
+      subArr = [];
+    }
   }
+
+  if (subArr.length) {
+    result.push(subArr);
+  }
+
+  return result;
 };
 
-const result = isEmpty({});
+const result = chunk([1, 2, 3, 4, 5], 2);
 
 console.log("result=>", result);
