15bc4e76d942926ecc5d884c8a8ab125374705f8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-10 10:23:32 +0600

    solve chunk problem with slice method

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,13 @@
 var chunk = function (arr, size) {
   const result = [];
-  let subArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    subArr.push(arr[i]);
-    if (subArr.length === size) {
-      result.push(subArr);
-      subArr = [];
-    }
-  }
-
-  if (subArr.length) {
-    result.push(subArr);
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
   }
 
   return result;
 };
 
-const result = chunk([1, 2, 3, 4, 5], 2);
+const result = chunk([1, 2, 3, 4, 5], 1);
 
 console.log("result=>", result);
