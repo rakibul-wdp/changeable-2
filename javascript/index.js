63e156fc264062f4e58c1290d2f966a23a622cba
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-10 10:29:57 +0600

    valid sudoko with js wrong

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,18 @@
-var chunk = function (arr, size) {
-  const result = [];
-
-  for (let i = 0; i < arr.length; i += size) {
-    result.push(arr.slice(i, i + size));
-  }
-
-  return result;
+var isValidSudoku = function (strs) {
+  return strs.map((str) => `${str.length}#${str}`).join("");
 };
 
-const result = chunk([1, 2, 3, 4, 5], 1);
+const board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+const result = isValidSudoku(board);
 
 console.log("result=>", result);
