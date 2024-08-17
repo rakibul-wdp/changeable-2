c2b7d82fd01090c6d7d8b64a176cc45a4c3ff75b
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-18 04:39:00 +0600

    create grow animation component

---
+++ src/components/GrowAnimation.js
@@ -0,0 +1,9 @@
+const GrowAnimation = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-24 h-24 bg-blueviolet rounded-full animate-pulse"></div>
+    </div>
+  );
+};
+
+export default GrowAnimation;
