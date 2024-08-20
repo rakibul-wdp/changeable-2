4de159da3c2166092066d91dd29236b3ee7f0207
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-21 02:39:00 +0600

    create sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="bg-gray-300 p-4">
+      <h3>Formatting</h3>
+      <button className="btn">Bold</button>
+      <button className="btn">Italic</button>
+    </div>
+  );
+};
+
+export default Sidebar;
