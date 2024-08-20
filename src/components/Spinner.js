39a1326a1b5e1f519ff93fa5dd62d18baf908258
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-21 03:39:00 +0600

    create spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-16 h-16 border-4 border-t-4 border-blueviolet border-solid rounded-full animate-spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
