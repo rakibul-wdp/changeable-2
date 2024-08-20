cc0b18f1987f4656474fc4bb3c7473dd5b505dfb
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-21 01:39:00 +0600

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="text-center my-6">
+      <h1 className="text-4xl font-bold text-blueviolet">
+        Spreadsheet Application
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
